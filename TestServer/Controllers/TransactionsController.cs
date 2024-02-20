4f76c8c8d818066a3d13fbaba959d631e960dc1d
Dimitrios Zervas <dimitri_zervas@hotmail.com>
2024-02-20 20:17:24 +0200

    Add project files.

---
+++ TestServer/Controllers/TransactionsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Transactions;
+
+namespace TestServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpPost]
+        [Route("PostTransaction")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> PostTransaction()
+        {
+            string body = "";
+            using (var reader = new StreamReader(Request.Body))
+            {
+                body = await reader.ReadToEndAsync();
+                Console.WriteLine(body);
+            }
+
+            return Ok("Hello!");
+        }
+    }
+}
